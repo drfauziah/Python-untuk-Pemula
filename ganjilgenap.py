5b1d47faff694436621b5a3ff503f5b8585fd71d
drfauziah <dfauziah99@gmail.com>
2020-01-07 13:49:28 +0700

    Memberi keterangan ganjil/genap

---
+++ ganjilgenap.py
@@ -0,0 +1,6 @@
+bil = int(input('Masukkan angka : '))
+if bil%2 == 0 :
+    print('Angka yang anda masukkan adalah', bil, ',', bil, 'merupakan angka genap.')
+else :
+    print('Angka yang anda masukkan adalah', bil, ',', bil, 'merupakan angka ganjil.')
+
