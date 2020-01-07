e2dea433b0ba1eca168ad5b45d77bd07c981be7b
drfauziah <dfauziah99@gmail.com>
2020-01-07 13:53:24 +0700

    Mengurutkan dari yang terbesar

---
+++ urutan_dari_terbesar.py
@@ -0,0 +1,9 @@
+angka1 = input('angka pertama = ')
+angka2 = input('angka kedua = ')
+
+#Mengurutkan angka
+print("Urutan angka dari yang terbesar sebagai berikut:")
+if angka1>angka2:
+    print(angka1, ',',angka2)
+else:
+    print(angka2, ',',angka1)
