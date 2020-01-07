bc678c690fa1b70f00fa95e79b5ac74a0218756f
Dwi Rizqi Fauziah <50840553+drfauziah@users.noreply.github.com>
2020-01-07 13:55:08 +0700

    Delete ganjil genap.py

--- ganjil genap.py
+++
@@ -1,6 +0,0 @@
-bil = int(input('Masukkan angka : '))
-if bil%2 == 0 :
-    print('Angka yang anda masukkan adalah', bil, ',', bil, 'merupakan angka genap.')
-else :
-    print('Angka yang anda masukkan adalah', bil, ',', bil, 'merupakan angka ganjil.')
-
